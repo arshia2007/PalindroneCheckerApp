57c1e8588427199da2c50de7e917ab24264803b9
Arshia <guptaarshia1@gmail.com>
2026-02-20 09:01:06 +0530

    Added UC1

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,9 @@
+import java.util.logging.SocketHandler;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("version 1.0");
+        System.out.println("USECASE1 - Welcome Page");
     }
 }
